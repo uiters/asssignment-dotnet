67de584928a10a097b218ef28986c83a5449904d
Phạm Trung Trường <32018323+trungtruongpham@users.noreply.github.com>
2020-04-17 15:28:18 +0700

    finish-assignment-ado-webapi (#9)

---
+++ lab5/assignment-ado-webapi/MaintainProduct/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace MaintainProduct
+{
+    internal class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public float UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ lab5/assignment-ado-webapi/RemoteServiceCore/Model/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoteServiceCore.Model
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public float UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
+    }
+
+    public static class ProductData
+    {
+        public static List<Product> ProductList = new List<Product>()
+        {
+            new Product{ProductID =1, ProductName = "CaPhe",Quantity= 10, UnitPrice=20},
+            new Product{ProductID=2, ProductName = "Coca", Quantity=20,UnitPrice=30 }
+        };
+    }
+}
