47c2a3194bb6dd5d83a0a9c3077921f44fc2ed9d
meantg <43744275+meantg@users.noreply.github.com>
2020-04-23 10:30:20 +0700

    BookManager
    Lab_1

---
+++ lab5/BookWebForm/MaintainBooks.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MaintainBooks.aspx.cs" Inherits="BookWebForm.MaintainBooks" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="BookID"></asp:Label>
+        <asp:TextBox ID="txtBookID" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="BookTitle"></asp:Label>
+            <asp:TextBox ID="txtBookTitle" runat="server"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label3" runat="server" Text="BookPrice"></asp:Label>
+        <asp:TextBox ID="txtBookPrice" runat="server"></asp:TextBox>
+        <p>
+            <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" />
+            <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" />
+            <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+        </p>
+        <asp:GridView ID="gvBookList" runat="server" AutoGenerateSelectButton="True" OnSelectedIndexChanging="gvBookList_SelectedIndexChanging">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ lab5/BookWebForm/MaintainBooks.aspx.cs
@@ -0,0 +1,64 @@
+﻿using BookWebForm.localhost;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BookWebForm
+{
+    public partial class MaintainBooks : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                LoadData();
+        }
+
+        public void LoadData()
+        {
+            BookService BookData = new BookService();
+            DataSet dsBooks = BookData.GetBookList();
+            gvBookList.DataSource = dsBooks.Tables[0];
+            gvBookList.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            BookService BookData = new BookService();
+            int BookID = int.Parse(txtBookID.Text);
+            string BookTitle = txtBookTitle.Text;
+            float BookPrice = float.Parse(txtBookPrice.Text);
+            BookData.AddBook(BookID, BookTitle, BookPrice);
+            LoadData();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            BookService BookData = new BookService();
+            int BookID = int.Parse(txtBookID.Text);
+            string BookTitle = txtBookTitle.Text;
+            float BookPrice = float.Parse(txtBookPrice.Text);
+            BookData.UpdateBook(BookID, BookTitle, BookPrice);
+            LoadData();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            BookService BookData = new BookService();
+            int BookID = int.Parse(txtBookID.Text);
+            BookData.DeleteBook(BookID);
+            LoadData();
+        }
+
+        protected void gvBookList_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+            GridViewRow row = gvBookList.Rows[e.NewSelectedIndex];
+            txtBookID.Text = row.Cells[1].Text;
+            txtBookTitle.Text = row.Cells[2].Text;
+            txtBookPrice.Text = row.Cells[3].Text;
+        }
+    }
+}
